f8cf1e1669a340a9a498702f2abef4e0880d4542
Adam Lovatt <jalovatt@hotmail.com>
2018-12-06 14:07:05 -0700

    Implement login/out

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
