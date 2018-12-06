db9f69530caa296895e20ecd39c9912329aab72a
Adam Lovatt <jalovatt@hotmail.com>
2018-12-06 14:35:48 -0700

    Use permanent rather than session cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to :root
     else
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  attr_accessor :remember_token
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true,
@@ -10,5 +12,30 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
   has_secure_password
+
+  def User.digest(str)
+    cost = ActiveModel::SecurePassword.min_cost ?
+            BCrypt::Engine::MIN_COST :
+            BCrypt::Engine.cost
+
+    BCrypt::Password.create(str, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
 end
